f8d002282e83cb736830eaebae466315dc4b581f
Ademods2 <106920368+Ademods2@users.noreply.github.com>
2022-06-18 21:54:54 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,16 +21,16 @@ global.auto_welcome_msg = true
 global.auto_leave_msg = true
 global.prefix = "."
 global.language = "ind"
-global.ownerNumberr = "62887435047326"
-global.ownerbot = ['62887435047326@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.ownerNumberr = "628980330708"
+global.ownerbot = ['628980330708@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
+global.ownername = "ModsBOT"
 global.botname = "Alphabot"
 global.thumb = "thumbnail_1.jpg"
 global.thumbnail ="thumbnail_2_1.jpg"
 global.fakethumb ="thumbnail_1.jpg"
 global.donasi ="donasi.jpg"
 global.captionpay ="Scan untuk melakukan pembayaran"
-global.fake = "「  ALPHABOT  」\nCreated By ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.fake = "「  ALPHABOT  」\nCreated By ModsBOT"
 global.alphaVersion = [2, 2143, 3] //WhatsApp web version
 
 /* Available type menu
